07f0c6c5e6ba40890c611a181dee8e24f69164f3
erenamanturov <erenamanturov@gmail.com>
2022-07-26 15:53:22 +0300

    qwerty

--- SecondMonth/OOP/test.py
+++ SecondMonth/OOP/test.py
@@ -5,11 +5,13 @@
 # a = [1, 3, 4, 3,5 ,4, 6,5, 7,7, 7]
 # a = str(a)
 # print(a)
-
-def dna_to_rna(dna):
-    for i in dna:
-        dna = i.replace('T', 'U')
-    return dna
-
-
-print(dna_to_rna('TTTT'))
+# def dna_to_rna(dna):
+#     for i in dna:
+#         dna = i.replace('T', 'U')
+#     return dna
+#
+#
+# print(dna_to_rna('TTTT'))
+x=int(input())
+for i in range(x):
+    print('%s%s' % (' ' * (x-i-1), '*' * (i*2+1)))
