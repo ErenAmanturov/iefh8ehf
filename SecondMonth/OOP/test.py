d364c7f8b2c0238c811c4a26af84f83e84279891
erenamanturov <erenamanturov@gmail.com>
2022-07-26 18:21:50 +0300

    qwerty

--- SecondMonth/OOP/test.py
+++ SecondMonth/OOP/test.py
@@ -11,7 +11,8 @@
 #     return dna
 #
 #
+
 # print(dna_to_rna('TTTT'))
 # x = int(input())
 # for i in range(x):
-#     print('%s%s' % (' ' * (x-i-1), '*' * (i*2+1)))
+#   print('%s%s' % (' ' * ( x - i - 1), 'x' * (i * 2 + 1)))
